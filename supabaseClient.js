45ef885c0e768b368453b2a3b2348345f8f54e32
abrorahmeed <abrorahmeed@gmail.com>
2025-08-09 19:43:39 +0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
-const { createClient } = supabase;
-const supabaseUrl = "https://bluumfpcmpqxvjfkxsoa.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsdXVtZnBjbXBxeHZqZmt4c29hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NDg2NzcsImV4cCI6MjA3MDMyNDY3N30.gFDDpkZgV8H40909xGebbR6DLCrBA8er0i4n2hf68qk";
+import { createClient } from '@supabase/supabase-js'
 
-window.supabaseClient = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://bluumfpcmpqxvjfkxsoa.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
