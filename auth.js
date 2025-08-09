022db82ff2de4d74658e765bee5e5d7bbf19d9b9
abrorahmeed <abrorahmeed@gmail.com>
2025-08-09 19:57:23 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,9 @@
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
-  
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
   const { data, error } = await supabaseClient.auth.signInWithPassword({
     email,
     password
