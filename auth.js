628821c32d03cb72fcdbae00cbf611e6ab68df91
abrorahmeed <abrorahmeed@gmail.com>
2025-08-09 19:31:35 +0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,17 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert("Login failed: " + error.message);
+  } else {
+    window.location.href = "dashboard.html";
+  }
+});
